b960cc3bb765d3e2ba771346c3790d1b39b9b0d1
yash-nomadic <yashsingh4537@gmail.com>
2025-01-21 14:09:35 +0530

    javascript index

---
+++ javascript/index.js
@@ -0,0 +1,22 @@
+console.log('Hello JavaScript');
+
+num=56.363;
+
+console.log(num);
+
+console.log(typeof num);
+
+myname= "Yash";
+
+console.log(myname);
+
+// new arithmatic operators
+console.log(9**3);
+
+console.log(1 + 1);
+console.log(1 == 1);
+console.log('1' + '1')
+console.log('1' == false)
+console.log('1' == '1')
+console.log('1' == 1)
+
