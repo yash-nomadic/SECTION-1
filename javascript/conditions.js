6bc02d5f7465a0eda02a1a08d8120f3412bcfa44
yash-nomadic <yashsingh4537@gmail.com>
2025-01-22 16:59:53 +0530

    conditions JavaScript

--- javascript/conditions.js
+++ javascript/conditions.js
@@ -1,15 +1,54 @@
-num= 68;
+num = 68;
 
-if(num%2===0){
+if (num % 2 === 0) {
     console.log('even');
 
-    msg='hello';
+    msg = 'hello';
 
-}else{
+    let msg2 = 'nice day for coding';
+    const pie = 3.14;
+    // pie=3487239679;
+} else {
     console.log('odd')
 }
 
 
 console.log(msg);
+// console.log(msg2);
+
+
+//Wap to check if a person eligiable for DL
+
+age = 20;
+
+if (age >= 18) {
+    console.log('eligiable for DL');
+
+
+} else {
+    console.log('not eligiable for DL');
+
+}
+
+
+const marks = 84;
+
+if (marks <= 100 && marks >= 90) {
+    console.log('Grade A')
+
+} else if (marks <= 89 && marks >= 70) {
+    console.log('Grade B');
+
+} else if (marks < 70 && marks >= 50) {
+    console.log('Grade C');
+
+} else if (marks, 50 && marks >= 0) {
+    console.log('Fail')
+
+} else {
+    console.log('invalid marks');
+
+}
+
 
 
