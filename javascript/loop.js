012a18b36813a232408cc3d42d7ed65ad016ff54
yash-nomadic <yashsingh4537@gmail.com>
2025-01-22 17:01:07 +0530

    loop in JavaScript

---
+++ javascript/loop.js
@@ -0,0 +1,25 @@
+// console.time('loop');
+for(let i=0; i<10; i++){
+    console.log(i);
+    
+}
+// console.timeLog('loop');    
+
+let a=10;
+
+while(a<30){
+    console.log(a);
+    a++;
+    // a+=1;
+    //a += 1;
+}
+
+console.log('do while loop');
+
+let b=20;
+
+do{
+    console.log(b);
+    b++;
+    
+}while(b<20);
