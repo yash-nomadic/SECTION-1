5772e3e53363d9e8a42d068bef9d7c55cf0b8f80
yash-nomadic <yashsingh4537@gmail.com>
2025-01-23 13:22:53 +0530

    functions in js

---
+++ javascript/function.js
@@ -0,0 +1,19 @@
+function addNums(a,b){
+    var c= a+b;
+    console.log(c);
+}
+addNums(89,55555);
+// console.log(c);
+
+
+const getAvg= function(a,b,c){
+    const avg= (a+b+c)/3;
+    return avg;
+    // console.log(avg);
+    
+};
+
+const result=getAvg(11,22,33);
+console.log(result);
+
+
